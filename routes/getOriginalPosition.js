7dc360b3d7afc02b06741ab5aabee0231049ea1f
linjunfeng <linjunfeng@kankan.com>
2016-06-07 18:37:34 +0800

    fix 502 error when .map file is not json format

--- routes/getOriginalPosition.js
+++ routes/getOriginalPosition.js
@@ -9,15 +9,20 @@ router.get('/', function(req, res, next) {
 	if (req.isAuthenticated()) {
 		var row = +req.query.row || -1;
 		var col = +req.query.col || -1;
-		var source_map_src = req.query.source_map_src || '';
+		var source_map_src = (req.query.source_map_src && decodeURIComponent(req.query.source_map_src)) || '';
 
 		if (row > 0 && col > 0 && source_map_src) {
 			request(source_map_src, function (error, response, body) {
 			    if (!error && response.statusCode == 200) {
-			    	var consumer = new sourceMap.SourceMapConsumer(body);
-			    	var result = consumer.originalPositionFor({ line: row, column: col });
+			    	try {
+			    		JSON.parse(body);
+			    		var consumer = new sourceMap.SourceMapConsumer(body);
+			    		var result = consumer.originalPositionFor({ line: row, column: col });
 
-			    	res.status(200).send(JSON.stringify({rtn: 0, message: result}));
+			    		res.status(200).send(JSON.stringify({rtn: 0, message: result}));
+			    	} catch (e) {
+			    		res.status(200).send(JSON.stringify({rtn: -1, message: 'the content of .map file is not json!'}));
+			    	}
 			    } else {
 			    	res.status(200).send(JSON.stringify({rtn: response.statusCode, message: response.statusMessage}));
 			    }
